b9784359b06e6884c10a8a05ed9c41b2fe4aec8b
kcc40 <59624419+kcc40@users.noreply.github.com>
2022-11-29 14:21:15 -0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -14,8 +14,8 @@ class Move():
     def draw(self, c):
         zoff = -4.5 - (2.5*c/52)
         zoff = round(zoff,2)
-        f = ["G1X125Y0Z0F5000\n", "S1000M03\n", f"G1X125Y0Z{zoff}F100\n", "G04P2\n",
-             "G1X125Y0Z0F200\n", "G1X0Y0Z0F5000\n", "M05\n", "G04P2\n"]
+        f = ["G1X125Y0Z0F5000", "G04P0", "S1000M03", "G04P0", f"G1X125Y0Z{zoff}F100", "G04P0", "G04P2", "G04P0",
+             "G1X125Y0Z0F200", "G04P0", "G1X0Y0Z0F5000", "G04P0", "M05", "G04P0", "G04P2", "G04P0"]
         # Stream g-code to grbl
         for line in f:
             l = line.strip()  # Strip all EOL characters for consistency
@@ -30,21 +30,18 @@ class Move():
     # dealer is only invoked on the deal -> no flipping
     # frate = feedrate, speed at which the move is executed
     def place(self, coord2, coord1=[0, 0], dealer=False, frate=5000):
-        f = [f"G01X{coord1[0]}Y{coord1[1]}Z0F{frate}\n", "M03\n",
-             f"G1X{coord1[0]}Y{coord1[1]}Z-7.0F100\n", "G04P2\n", f"G01X{coord1[0]}Y{coord1[1]}Z0F{200}\n"]
+        f = [f"G01X{coord1[0]}Y{coord1[1]}Z0F{frate}", "M03",
+             f"G1X{coord1[0]}Y{coord1[1]}Z-7.0F100", "G04P2", f"G01X{coord1[0]}Y{coord1[1]}Z0F200"]
 #         if dealer:
-#             f = ["G1X0Y0Z0F5000\n", "M03\n",
-#                  "G1X0Y0Z-7.0F100\n", "G04P2\n", "G1X0Y0Z0F200\n"]
+#             f = ["G1X0Y0Z0F5000", "M03",
+#                  "G1X0Y0Z-7.0F100", "G04P2", "G1X0Y0Z0F200"]
 #         else:
-#             f = ["G1X0Y0Z0F5000\n", "M03\n",
-#                  "G1X0Y0Z-7.0F100\n", "G04P2\n", "G1X0Y0Z0F200\n"]
-# #             f = ["flip\n", "unflip\n", "flipped\n", "M03\n",
-# #                  "flipped + z\n", "G04P2\n", "flipped\n"]
-        d0 = f"G01X{coord2[0]}Y{coord2[1]}Z0F{frate}\n"
-        d1 = "M05\n"
-        p1 = "G04P2\n"
-        d2 = "G1X125Y0Z0F5000\n"
-        f.extend([d0, d1, p1, d2])
+#             f = ["G1X0Y0Z0F5000", "M03",
+#                  "G1X0Y0Z-7.0F100", "G04P2", "G1X0Y0Z0F200"]
+# #             f = ["flip", "unflip", "flipped", "M03",
+# #                  "flipped + z", "G04P2", "flipped"]
+        d = [f"G01X{coord2[0]}Y{coord2[1]}Z0F{frate}", "M05", "G04P2", "G1X125Y0Z0F5000"]
+        f.extend(d)
 
         # Stream g-code to grbl
         for line in f:
@@ -59,11 +56,7 @@ class Move():
     # c is a stack of every location a card has been placed; each entry contains a list [x,y]
     def discard(self, stack, frate=5000):
         for c in stack:
-            f = [f"G01X{c[0]}Y{c[1]}Z0F{frate}\n","S1000M03\n",f"G01X{c[0]}Y{c[1]}Z-7.0F100\n","G04P2\n",f"G01X{c[0]}Y{c[1]}Z0F{frate}\n","G1X250Y0Z0\n","M05\n","G04P2\n"]
-#             von = "S1000M03\n"
-#             d1 = f"G01X{c[0]}Y{c[1]}Z-7.0F100\nG04P2\n"
-#             d2 = "G1X250Y0Z0\nM05\nG04P2\n"
-#             f.extend([von, d1, f, d2])
+            f = [f"G01X{c[0]}Y{c[1]}Z0F{frate}","S1000M03",f"G01X{c[0]}Y{c[1]}Z-7.0F100\n","G04P2",f"G01X{c[0]}Y{c[1]}Z0F200","G1X250Y0Z0\n","M05","G04P2"]
             for line in f:
                 l = line.strip()  # Strip all EOL characters for consistency
                 print('Sending: ' + l)
