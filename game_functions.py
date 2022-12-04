f4ee6b195f7f0b24d1e4a9d8631905d026bc83fa
Curtis Maher <18maherc@gmail.com>
2022-12-04 18:15:46 -0500

    missed some stuff

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,4 @@
-import blackjack
+from game_objects import *
 from communication import Move
 import mainDetector
 move = Move()
@@ -7,7 +7,7 @@ move = Move()
 # ---- Game Functions: ----
 
 # function prompting the Player to Hit or Stand
-def action(x: str, deck: blackjack.Deck, hand: blackjack.Hand, player: blackjack.Player, move: Move):
+def action(x: str, deck: Deck, hand: Hand, player: Player, move: Move):
     try:
         if x[0].lower() == 'h':
             hit(deck, hand, move)
@@ -25,23 +25,23 @@ def action(x: str, deck: blackjack.Deck, hand: blackjack.Hand, player: blackjack
         print(e)
 
 
-def hit(deck: blackjack.Deck, hand: blackjack.Hand, move: Move):
-    move.draw(len(blackjack.card_stack))
+def hit(deck: Deck, hand: Hand, move: Move):
+    move.draw(len(card_stack))
     hand.add_card(mainDetector.getCard())
     move.place(hand.cards[-1].coords)
 
 
-def stand(hand: blackjack.Hand):
+def stand(hand: Hand):
     hand.done_flag = True
 
 
-def split(deck: blackjack.Deck, player: blackjack.Player, move: Move):
+def split(deck: Deck, player: Player, move: Move):
     player.split_hand(move)
     hit(deck, player.hands[0], move)
     hit(deck, player.hands[1], move)
 
 
-def double(deck: blackjack.Deck, hand: blackjack.Hand, player: blackjack.Player, move: Move):
+def double(deck: Deck, hand: Hand, player: Player, move: Move):
     if len(hand.cards) == 2:
         if player.wallet >= player.wager:
             player.remove_credits(player.wager)
@@ -54,7 +54,7 @@ def double(deck: blackjack.Deck, hand: blackjack.Hand, player: blackjack.Player,
         raise Exception("Can only double down with a 2 card hand")
 
 
-def surrender(hand: blackjack.Hand):
+def surrender(hand: Hand):
     if len(hand.cards) == 2:
         print("Player surrenders this hand")
         hand.surrender_flag = True
@@ -63,7 +63,7 @@ def surrender(hand: blackjack.Hand):
         raise Exception("Can only surrender on initial hand")
 
 
-def insurance(player: blackjack.Player):
+def insurance(player: Player):
     the_wager = player.hands[0].wager
     if player.wallet >= 0.5*the_wager:
         player.insurance_flag = True
@@ -73,18 +73,18 @@ def insurance(player: blackjack.Player):
 
 
 # --- functions to display cards ---
-def show_dealer(dealer_hand: blackjack.Hand):
+def show_dealer(dealer_hand: Hand):
     print(f"\nDealer's Hand: {dealer_hand.score}")
     for card in dealer_hand.cards:
         print(f"{card.to_string()}, ")
 
 
-def show_dealer_hidden(dealer_hand: blackjack.Hand):
+def show_dealer_hidden(dealer_hand: Hand):
     print("\nDealer's Hand: ")
     print(f"{dealer_hand.cards[0].to_string()}, <hidden>")
 
 
-def show_player(player_num: int, player_hand: blackjack.Hand):
+def show_player(player_num: int, player_hand: Hand):
     """Show the player's cards
 
     Args:
@@ -97,43 +97,43 @@ def show_player(player_num: int, player_hand: blackjack.Hand):
 
 
 # --- functions to handle end of game scenarios ---
-def player_busts(player: blackjack.Player) -> str:
+def player_busts(player: Player) -> str:
     print("Player busts!")
     player.add_credits(0)
     return 'lose'
 
 
-def player_wins(player: blackjack.Player) -> str:
+def player_wins(player: Player) -> str:
     print("Player wins!")
     player.add_credits(2*player.wager)
     return 'win'
 
 
-def player_blackjack(player: blackjack.Player) -> str:
+def player_blackjack(player: Player) -> str:
     print("Player won with blackjack!")
     player.add_credits(2.5*player.wager)
     return 'win'
 
 
-def dealer_busts(player: blackjack.Player) -> str:
+def dealer_busts(player: Player) -> str:
     print("Dealer busts!")
     player.add_credits(2*player.wager)
     return 'win'
 
 
-def dealer_wins(player: blackjack.Player) -> str:
+def dealer_wins(player: Player) -> str:
     print("Dealer wins!")
     player.add_credits(0)
     return 'lose'
 
 
-def push(player: blackjack.Player) -> str:
+def push(player: Player) -> str:
     print("Dealer and Player tie! It's a push.")
     player.add_credits(player.wager)
     return 'push'
 
 
-def calculate_winner(dealer_hand: blackjack.Hand, player_hand: blackjack.Hand, player: blackjack.Player) -> str:
+def calculate_winner(dealer_hand: Hand, player_hand: Hand, player: Player) -> str:
     # Player hit 21
     if player_hand.score == 21:
         if player_hand.size == 2:
