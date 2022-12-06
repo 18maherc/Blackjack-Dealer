481291723bf56de41580236f44da8a6043955ba1
kcc40 <59624419+kcc40@users.noreply.github.com>
2022-12-06 17:33:51 -0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -5,17 +5,22 @@ import time
 class Move():
     def __init__(self):
         # Open grbl serial port, 'COM13' will need replaced with the corresponding RPi port
-        self.s = serial.Serial('COM3', 115200)
+        # ls /dev/ttyACM*
+        try:
+            self.s = serial.Serial('/dev/ttyACM0', 115200)
+        except FileNotFoundError:
+            self.s = serial.Serial('/dev/ttyACM1', 115200)
+
         # Wake up grbl
         self.s.write(str.encode('\r\n\r\n'))
         time.sleep(2)   # Wait for grbl to initialize
         self.s.flushInput()  # Flush startup text in serial input
 
-    def draw(self, c):
-        zoff = -4.5 - (2.5*c/52)
-        zoff = round(zoff,2)
-        f = ["G1X125Y0Z0F5000\n", "S1000M03\n", f"G1X125Y0Z{zoff}F100\n", "G04P2\n",
-             "G1X125Y0Z0F200\n", "G1X0Y0Z0F5000\n", "M05\n", "G04P2\n"]
+    def draw(self, c, frate=10000):
+        zoff = -4.7 - (1.3*c/52)
+        zoff = round(zoff, 1)
+        f = [f"G1X125Y0Z-1F{frate}", "G04P0", "S1000M03", "G04P0", f"G1X125Y0Z{zoff}F200", "G04P0", "G04P1", "G04P0",
+             "G1X125Y0Z-1F200", "G04P0", f"G1X0Y0Z-1F{frate}", "G04P0", "G0X0Y0Z-4.5", "G04P0", "M05", "G04P0", "G04P2", "G04P0", "M03", "G0X70Y0Z-1", "G04P0", "M05", "G04P0"]
         # Stream g-code to grbl
         for line in f:
             l = line.strip()  # Strip all EOL characters for consistency
@@ -29,23 +34,41 @@ class Move():
     # coord is the location the card needs to be placed; a list consisting of 2 values [x,y]
     # dealer is only invoked on the deal -> no flipping
     # frate = feedrate, speed at which the move is executed
-    def place(self, coord2, coord1=[0, 0], dealer=False, frate=5000):
-        f = [f"G01X{coord1[0]}Y{coord1[1]}Z0F{frate}\n", "M03\n",
-             f"G1X{coord1[0]}Y{coord1[1]}Z-7.0F100\n", "G04P2\n", f"G01X{coord1[0]}Y{coord1[1]}Z0F{200}\n"]
-#         if dealer:
-#             f = ["G1X0Y0Z0F5000\n", "M03\n",
-#                  "G1X0Y0Z-7.0F100\n", "G04P2\n", "G1X0Y0Z0F200\n"]
-#         else:
-#             f = ["G1X0Y0Z0F5000\n", "M03\n",
-#                  "G1X0Y0Z-7.0F100\n", "G04P2\n", "G1X0Y0Z0F200\n"]
-# #             f = ["flip\n", "unflip\n", "flipped\n", "M03\n",
-# #                  "flipped + z\n", "G04P2\n", "flipped\n"]
-        d0 = f"G01X{coord2[0]}Y{coord2[1]}Z0F{frate}\n"
-        d1 = "M05\n"
-        p1 = "G04P2\n"
-        d2 = "G1X125Y0Z0F5000\n"
-        f.extend([d0, d1, p1, d2])
+    def deal(self, coord2, dealer=False, frate=10000):
+        f = []
+        if dealer:
+            coord1=[0, 0]
+        else:
+            coord1=[0, -85]
+            f = ["G0Z-16", "G4P2", "G0Z-1"]
+            
+        coord2[1] = -coord2[1]
+        d = [f"G01X{coord1[0]}Y{coord1[1]}Z-1F{frate}", "M03",
+             f"G1X{coord1[0]}Y{coord1[1]}Z-6.0F200", "G04P1", f"G0X{coord1[0]}Y{coord1[1]}Z-1",
+             f"G01X{coord2[0]}Y{coord2[1]}Z-1F{frate}", f"G0X{coord2[0]}Y{coord2[1]}Z-4.5",
+             "M05", "G04P1", "M3", f"G0X{coord2[0]}Y{coord2[1]}Z-1", "M5", f"G1X125Y0Z-1F{frate}"]
+        f.extend(d)
 
+        # Stream g-code to grbl
+        for line in f:
+            l = line.strip()  # Strip all EOL characters for consistency
+            print('Sending: ' + l)
+            self.s.write(str.encode(l + '\n'))  # Send g-code block to grbl
+            grbl_out = self.s.readline()  # Wait for grbl response with carriage return
+            print(' : ' + bytes.decode(grbl_out.strip()))
+        print('Stream Complete')
+        
+    # s is the serial variable
+    # coord is the location the card needs to be placed; a list consisting of 2 values [x,y]
+    # dealer is only invoked on the deal -> no flipping
+    # frate = feedrate, speed at which the move is executed
+    def place(self, coord2, coord1, frate=10000):
+        coord1[1] = -coord1[1]
+        coord2[1] = -coord2[1]
+        f = [f"G01X{coord1[0]}Y{coord1[1]}Z-1F{frate}", "M03",
+             f"G1X{coord1[0]}Y{coord1[1]}Z-6.0F200", "G04P1", f"G0X{coord1[0]}Y{coord1[1]}Z-1",
+             f"G01X{coord2[0]}Y{coord2[1]}Z-1F{frate}", f"G0X{coord2[0]}Y{coord2[1]}Z-4.5",
+             "M05", "G04P1", "M3", f"G0X{coord2[0]}Y{coord2[1]}Z-1", "M5", f"G1X125Y0Z-1F{frate}"]
         # Stream g-code to grbl
         for line in f:
             l = line.strip()  # Strip all EOL characters for consistency
@@ -57,24 +80,27 @@ class Move():
 
     # s is the serial variable
     # c is a stack of every location a card has been placed; each entry contains a list [x,y]
-    def discard(self, stack, frate=5000):
+    def discard(self, stack, frate=10000):
         for c in stack:
-            f = [f"G01X{c[0]}Y{c[1]}Z0F{frate}\n","S1000M03\n",f"G01X{c[0]}Y{c[1]}Z-7.0F100\n","G04P2\n",f"G01X{c[0]}Y{c[1]}Z0F{frate}\n","G1X250Y0Z0\n","M05\n","G04P2\n"]
-#             von = "S1000M03\n"
-#             d1 = f"G01X{c[0]}Y{c[1]}Z-7.0F100\nG04P2\n"
-#             d2 = "G1X250Y0Z0\nM05\nG04P2\n"
-#             f.extend([von, d1, f, d2])
+            c[1] = c[1]
+            f = [f"G01X{c[0]}Y{c[1]}Z-1F{frate}", "S1000M03", f"G01X{c[0]}Y{c[1]}Z-6.0F200",
+                 "G04P2", f"G01X{c[0]}Y{c[1]}Z0F200", "G0X250Y-15Z0", "M05", "G04P2"]
             for line in f:
                 l = line.strip()  # Strip all EOL characters for consistency
                 print('Sending: ' + l)
                 self.s.write(str.encode(l + '\n'))  # Send g-code block to grbl
                 grbl_out = self.s.readline()  # Wait for grbl response with carriage return
                 print(' : ' + bytes.decode(grbl_out.strip()))
-            print('Stream Complete')
+        l = "G0X0Y0Z0"
+        print('Sending: ' + l)
+        self.s.write(str.encode(l + '\n'))  # Send g-code block to grbl
+        grbl_out = self.s.readline()  # Wait for grbl response with carriage return
+        print(' : ' + bytes.decode(grbl_out.strip()))
+        print('Stream Complete')
 
     def setG(self):
         f = ["$1=255", "$100=40", "$101=40",
-             "$102=100", "$110=5000", "$111=5000", "$120=100", "$121=100", "$122=4"]
+             "$102=100", "$110=10000", "$111=10000", "$120=300", "$121=300", "$122=4"]
         # Stream g-code to grbl
         for line in f:
             l = line.strip()  # Strip all EOL characters for consistency
