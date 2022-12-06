7e4b315661799cf7a309c2acb0f33226be9db113
Curtis Maher <18maherc@gmail.com>
2022-12-06 17:19:32 -0500

    commit message goes here

---
+++ game_tests.py
@@ -0,0 +1,72 @@
+import pytest
+from tests_objects import *
+
+
+class UnitTests:
+    testdeck = Deck()
+
+    blackjack = Hand()
+    blackjack.add_card(Card('Hearts', 'Ace'))
+    blackjack.add_card(Card('Hearts', 'King'))
+
+    fourteen = Hand()
+    fourteen.add_card(Card('Hearts', 'Eight'))
+    fourteen.add_card(Card('Hearts', 'Six'))
+
+    nineteen = Hand()
+    nineteen.add_card(Card('Hearts', 'Nine'))
+    nineteen.add_card(Card('Hearts', 'Ten'))
+
+    doubleace = Hand()
+    doubleace.add_card(Card('Hearts', 'Ace'))
+    doubleace.add_card(Card('Hearts', 'Ace'))
+
+    # Player gets blackjack
+    testDealer = Dealer(nineteen)
+    dealerhand = testDealer.hand
+    testplayer = Player()
+    testplayer.add_hand(blackjack)
+    playerhand = testplayer.hands[0]
+    blackjack.show_player(playerhand)
+    blackjack.calculate_winner(dealerhand, playerhand)
+
+    # Player and dealer both have blackjack
+    testDealer = Dealer(blackjack)
+    dealerhand = testDealer.hand
+    testplayer = Player()
+    testplayer.add_hand(blackjack)
+    playerhand = testplayer.hands[0]
+    blackjack.show_player(playerhand)
+    blackjack.calculate_winner(dealerhand, playerhand)
+
+    # Player busts - Dealer auto wins
+    testDealer = Dealer(nineteen)
+    dealerhand = testDealer.hand
+    testplayer = Player()
+    testplayer.add_hand(fourteen)
+    playerhand = testplayer.hands[0]
+    playerhand.add_card(Card('Hearts', 'Nine'))
+    blackjack.show_player(playerhand)
+    blackjack.calculate_winner(dealerhand, playerhand)
+
+    # Player stands, Dealer wins
+    testDealer = Dealer(nineteen)
+    dealerhand = testDealer.hand
+    testplayer = Player()
+    testplayer.add_hand(fourteen)
+    playerhand = testplayer.hands[0]
+    blackjack.show_player(playerhand)
+    blackjack.calculate_winner(dealerhand, playerhand)
+
+    # Player stands, Dealer busts
+    testDealer = Dealer(nineteen)
+    dealerhand = testDealer.hand
+    testplayer = Player()
+    testplayer.add_hand(nineteen)
+    playerhand = testplayer.hands[0]
+    dealerhand.add_card(Card('Hearts', 'Nine'))
+    blackjack.show_player(playerhand)
+    blackjack.calculate_winner(dealerhand, playerhand)
+
+    def testname(self):
+        assert self.fourteen.score == 14
