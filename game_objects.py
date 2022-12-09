310d6c835432aa552dbd500ed694c7e2fcdc68e6
18maherc <18maherc@gmail.com>
2022-12-08 22:40:24 -0500

    ya wanna know how i got these scarsssss

--- game_objects.py
+++ game_objects.py
@@ -13,11 +13,11 @@ from kivy.properties import ObjectProperty, NumericProperty
 
 
 # Let's give the info of the card's suits, ranks and values
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs', 'Joker')
 values = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace', 'Joker')
 points = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
-          'Queen': 10, 'King': 10, 'Ace': 11}
+          'Queen': 10, 'King': 10, 'Ace': 11, 'Joker': 69}
 card_stack = []
 
 # Declare all game/gui objects
